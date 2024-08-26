408002e9f1384ed0827e8fa188890f532151ebd0
ethpsdu <ethan.stapff@gmail.com>
2024-08-26 11:56:41 +0200

    Delta and circleshape.py

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from constants import *
 # Main Function
 def main():
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 # Can only be called if run directly
 if __name__ == "__main__":
     main()
